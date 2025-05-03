4bc6e0ccffbc7441bfb742572c4695b64089aa6d
alexander soriano aarseth <alexander.aarseth@visma.com>
2025-05-03 17:51:53 +0200

    register user endpoint

--- src/main/java/com/torshovlabs/quote/controller/UserController.java
+++ src/main/java/com/torshovlabs/quote/controller/UserController.java
@@ -1,30 +1,43 @@
 package com.torshovlabs.quote.controller;
 
 import com.torshovlabs.quote.controller.dto.RegisterUserDTO;
+import com.torshovlabs.quote.domain.User;
+import com.torshovlabs.quote.service.UserService;
+import com.torshovlabs.quote.util.exceptions.UsernameAlreadyExistsException;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RequestMapping("/api/user")
 @RestController
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
 
     /**
      * Payload from user to register account
-     * hence we need to keep this endpoint for backwards compatibility.
      */
-    @RequestMapping(value = {"/register", "/register.json"}, method = GET)
-    public ResponseEntity<String> registerUser(@Valid @RequestBody RegisterUserDTO registerUserDTO) {
-        String username = registerUserDTO.getUsername();
-
-        return ResponseEntity.ok("User registered with username: " + username);
+    @RequestMapping(value = {"/register", "/register.json"}, method = POST)
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterUserDTO registerUserDTO) {
+        try {
+            User registeredUser = userService.registerUser(registerUserDTO.getUsername());
+            return ResponseEntity.ok("User registered successfully with username: " + registeredUser.getName());
+        } catch (UsernameAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
     }
 
 }
--- src/main/java/com/torshovlabs/quote/service/UserService.java
+++ src/main/java/com/torshovlabs/quote/service/UserService.java
@@ -1,5 +1,37 @@
 package com.torshovlabs.quote.service;
 
+import com.torshovlabs.quote.dao.UserDAO;
+import com.torshovlabs.quote.domain.User;
+import com.torshovlabs.quote.util.exceptions.UsernameAlreadyExistsException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
 public class UserService {
 
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Transactional
+    public User registerUser(String username) {
+        // Check if username already exists
+        if (userDAO.existsByName(username)) {
+            throw new UsernameAlreadyExistsException("Username '" + username + "' is already taken");
+        }
+
+        // Create new user
+        User newUser = new User();
+        newUser.setId(UUID.randomUUID().toString());
+        newUser.setName(username);
+
+        // Save and return the user
+        return userDAO.save(newUser);
+    }
 }
